bf55019ba8961737b9939092006b5555acaebaa0
alfizzu <afibinthy@gmail.com>
2022-06-14 10:49:18 +0400

    added list file

---
+++ list/list_languages.py
@@ -0,0 +1,15 @@
+names=["c++","c","python","java","javascript"]
+
+# list languages
+
+print("one approach")
+for i in range(0,5):
+    print(names[i])
+
+print(" ")
+print("another way to list each element")
+
+for lang in names:
+    print(lang)
+
+
